22cd6324542e26f644d7ac98fb94f68dd6a8ad3c
Poujhit <poujhit.dev@gmail.com>
2021-11-27 17:31:35 +0530

    background color changed

--- frontend/src/components/Background.tsx
+++ frontend/src/components/Background.tsx
@@ -14,8 +14,7 @@ const Background: React.FC<BackgroundProps> = (props) => {
         alignItems: 'center',
         height: '100vh',
         width: '100vw',
-        backgroundImage:
-          'linear-gradient(180deg, #F3F3FB 0%, #F3F3FB 0.01%, #FDFBFD 100%)',
+        backgroundColor: '#171926',
         boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
       }}
     >
--- frontend/src/theme/Theme.ts
+++ frontend/src/theme/Theme.ts
@@ -4,4 +4,10 @@ export const theme = createTheme({
   typography: {
     fontFamily: 'TTNorms-Regular',
   },
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#16d741',
+    },
+  },
 });
