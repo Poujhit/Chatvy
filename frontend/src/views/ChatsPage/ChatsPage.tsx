802e951f3948603b86a055aed852e4ce7c904ff1
Poujhit <poujhit.dev@gmail.com>
2022-01-11 23:04:10 +0530

    Added chat room list and conversation details in the chat page

--- frontend/src/views/ChatsPage/ChatsPage.tsx
+++ frontend/src/views/ChatsPage/ChatsPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Background from 'components/Background';
-import { Typography } from '@mui/material';
+// import { Typography } from '@mui/material';
 import MainLayout from 'components/MainLayout/MainLayout';
 
 import { makeStyles } from 'utils/makeStyles';
+import ChatRoomList from './components/ChatRoomList';
+import ConverstionDetails from './components/ConverstionDetails';
 
 interface ChatsPageProps {}
 
@@ -11,6 +13,7 @@ const useStyles = makeStyles()(() => ({
   root: {
     height: '100%',
     // cursor: 'pointer',
+    width: '100%',
     display: 'flex',
     overflow: 'hidden',
   },
@@ -21,7 +24,10 @@ const ChatsPage: React.FC<ChatsPageProps> = (props) => {
   return (
     <Background isHome={true}>
       <MainLayout />
-      <div className={classes.root}></div>
+      <div className={classes.root}>
+        <ChatRoomList />
+        <ConverstionDetails />
+      </div>
     </Background>
   );
 };
